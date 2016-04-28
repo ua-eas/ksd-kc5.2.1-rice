87c7236506ec9642c9e870240c2b2b8d7bbd7836
Scott G. Skinner <sskinner@email.arizona.edu>
2016-04-28 16:31:48 -0700

    UAF-2097 - Correcting comment referencees and spelling, adding in missing return statement.

--- impl/src/main/java/org/kuali/rice/krad/web/listener/KualiHttpSessionListener.java
+++ impl/src/main/java/org/kuali/rice/krad/web/listener/KualiHttpSessionListener.java
@@ -15,21 +15,17 @@
  */
 package org.kuali.rice.krad.web.listener;
 
-import javax.servlet.http.HttpSessionEvent;
-import javax.servlet.http.HttpSessionListener;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-import org.kuali.rice.kew.api.exception.WorkflowException;
 import org.kuali.rice.kim.api.identity.Person;
 import org.kuali.rice.krad.UserSession;
-import org.kuali.rice.krad.document.Document;
 import org.kuali.rice.krad.document.authorization.PessimisticLock;
 import org.kuali.rice.krad.service.KRADServiceLocatorWeb;
-import org.kuali.rice.krad.util.GlobalVariables;
 import org.kuali.rice.krad.util.KRADConstants;
 import org.kuali.rice.krad.util.ObjectUtils;
 
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 import java.util.List;
 
 /**
@@ -58,7 +54,7 @@ public class KualiHttpSessionListener implements HttpSessionListener {
     /**
      * HttpSession hook for additional cleanup when sessions are destroyed.
      *
-     * UAR-2097: Ensure already destroyed sessions can'r casue STE's to fill up logs during
+     * UAF-2097: Ensure already destroyed sessions can't cause STE's to fill up logs during
      *           session clearing for memory events. This means return immediately when we see
      *           that we don't have enough info to clear doc locks.
      *
@@ -76,13 +72,14 @@ public class KualiHttpSessionListener implements HttpSessionListener {
 
         String sessionId = userSession.getKualiSessionId();
         if(StringUtils.isBlank(sessionId)){
-            LOG.warn("Could not realease document locks for null sessionId!");
+            LOG.warn("Could not realease document locks for because sessionId is null!");
             return;
         }
 
         List<PessimisticLock> locks = KRADServiceLocatorWeb.getPessimisticLockService().getPessimisticLocksForSession(sessionId);
         if(ObjectUtils.isNull(locks) || locks.size() <  1){
-            // Don't make more work if we don't need to, but not a warn event
+            // Don't make more work if we don't need to, but not necessarily a warn event since the user
+            // might not have any doc locks out
             return;
         }
 
@@ -90,6 +87,7 @@ public class KualiHttpSessionListener implements HttpSessionListener {
         if(ObjectUtils.isNull(user)){
             // Really paranoid about nulls causing stack traces that fill up logs
             LOG.warn("Person object returned null for sessionId: " + sessionId);
+            return;
         }
 
         // Ok, clear out any document locks
