fd926f35ff8980c092799a3bc24a6b43357f4ed4
sskinner <sskinner@email.arizona.edu>
2020-04-30 14:31:09 -0700

    FIN-1443-sgs - Updating log4j to use correct package in edu classes.

--- rice-middleware/impl/src/main/java/edu/arizona/rice/kew/actionrequest/service/impl/RspChangeRerouteDisabledActionRequestServiceImpl.java
+++ rice-middleware/impl/src/main/java/edu/arizona/rice/kew/actionrequest/service/impl/RspChangeRerouteDisabledActionRequestServiceImpl.java
@@ -2,8 +2,10 @@ package edu.arizona.rice.kew.actionrequest.service.impl;
 
 import java.util.Set;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.kuali.rice.kew.actionrequest.service.impl.ActionRequestServiceImpl;
-import org.apache.log4j.Logger;
+
 
 /**
  * This class overrides ActionRequestServiceImpl to disable edoc reroute by
@@ -11,12 +13,12 @@ import org.apache.log4j.Logger;
  */
 public class RspChangeRerouteDisabledActionRequestServiceImpl extends ActionRequestServiceImpl {
 
-	private static final Logger log = Logger.getLogger(RspChangeRerouteDisabledActionRequestServiceImpl.class);
+	private static final Logger LOG = LogManager.getLogger(RspChangeRerouteDisabledActionRequestServiceImpl.class);
 
 	@Override
 	public void updateActionRequestsForResponsibilityChange(Set<String> responsibilityIds) {
 
-		log.info("UA Implementation to skip rerouting/requeing edoc for Responsibility change processing.");
+		LOG.info("UA Implementation to skip rerouting/requeing edoc for Responsibility change processing.");
 
 	}
 
